25940c8258ef6b05bbd0552f7c960318eaf2ffa7
GarlicBuns19 <planterx26@gmail.com>
2022-10-12 19:54:22 +0100

    Shop

--- index.js
+++ index.js
@@ -2,24 +2,38 @@ const express = require("express");
 const app = express();
 const port = parseInt(process.env.PORT) || 3000;
 
-app.get('/',(req,res) => {
-    try{
-        res.json({
-            status : 200,
-            msg: "You are home"
-        })
-    }catch (e){
-        res.json({
-            status : 400,
-            msg : "Could not get homepage"
-        })
-    }
-})
+app.get("/", (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      msg: "You are home",
+    });
+  } catch (e) {
+    res.json({
+      status: 400,
+      msg: "Could not get homepage",
+    });
+  }
+});
 
-app.listen(port,(err) => {
-    if(err){
-        console.log(err)
-    }else{
-        console.log(`Sever is running on http://localhost:${port}`)
-    }
-})
+app.get("/products", (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      msg: "You in the shop",
+    });
+  } catch (e) {
+    res.json({
+      status: 400,
+      msg: "U could not get in shop",
+    });
+  }
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Sever is running on http://localhost:${port}`);
+  }
+});
